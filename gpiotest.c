09c740dc3f376976fcb147d116735a156ee49a76
Clayton G. Hobbs <clay@lakeserv.net>
2012-06-23 11:13:35 +0000

    Made gpiotest more Masterful
    ....  ....  ....  ....  ....  ....  ....  ....  ....  ....

--- gpiotest.c
+++ gpiotest.c
@@ -1,23 +1,25 @@
 // $ gcc -g -o gpiotest gpiotest.c -Lbuild -lbone -Isrc/
+// # LD_LIBRARY_PATH="build/" ./gpiotest
 #include <stdio.h>
 #include <unistd.h>
 #include "libbone.h"
 
 int main(void)
 {
-    int i, j;
+    int i, j, k;
 
     bone_gpio_export(P8+3);
     i = bone_gpio_get_dir(P8+3);
     bone_gpio_set_dir(P8+3, 1); /* Output */
+    bone_gpio_set_value(P8+3, 0);
     for (j = 0; j < 10; j++) {
-        i = bone_gpio_get_value(P8+3); /* See what it is right now */
-        if (i == 0) {
+        for (k = 0; k < 4; k++) {
             bone_gpio_set_value(P8+3, 1);
-        } else if (i == 1) {
+            usleep(6666);
             bone_gpio_set_value(P8+3, 0);
+            usleep(160000);
         }
-        usleep(500000);
+        usleep(333333);
     }
 
     bone_gpio_unexport(P8+3);
